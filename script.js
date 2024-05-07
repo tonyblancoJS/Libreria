d03bcf047bc9afb08e2090af1d9d24a03481da5f
Antonio Blanco <antonioblanco2072@gmail.com>
2024-05-07 00:15:28 -0300

    Elimine un repositor Biblioteca que estaba en esta rama, la deje a fuera

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var menuBtn = document.querySelector('.menu-btn');
     var menu = document.querySelector('.menu');
 
-    menuBtn.addEventListener('click', function() {
+    menuBtn.addEventListener('mouseover', function() {
         if (menu.style.display === 'none' || menu.style.display === '') {
             menu.style.display = 'block';
         } else {
             menu.style.display = 'none';
         }
     });
+
 });
