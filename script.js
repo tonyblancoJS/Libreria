96811b7bab7897c3baca5489cda57996de0fc88d
Antonio Blanco <antonioblanco2072@gmail.com>
2024-05-21 19:50:40 -0300

    se ajustaron las portadas al angostar el navegador para que las portadas bajen a la linea siguiente

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+const firstname = document.querySelector("#firstName");
+const lastname = document.querySelector('#lastName');
+const errorFirstname = document.querySelector('#error-firstname');
+const formRegister = document.querySelector('#formRegister');
+
+if(lastname && errorFirstname && formRegister){
+    formRegister.addEventListener('submit', validarFormulario)
+}else{
+    console.log("error!! no se puede manjar eventos no encontrados")
+}
+
+ function validarFormulario(event){
+    event.preventDefault();
+
+    let validation = true;
+
+    if(firstname.value === ''){
+        firstname.classList.add("error");
+        errorFirstname.textContent = "El nombre no puede estar vacío"
+        validation = false;
+        errorFirstname.display='block';
+    }
+ }
